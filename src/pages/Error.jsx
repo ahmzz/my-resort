d1dfb104719502b8ae5b787ffac9355fb9c3b471
ahmzz <ahmedminhas247@outlook.com>
2023-07-05 23:51:04 +0500

    Banners of all pages implemented

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import Hero from '../components/Hero'
+import Banner from '../components/Banner'
 const Error = () => {
   return (
-    <Hero/>
+    <Hero>
+      <Banner
+        title="404"
+        subtitle="Page not found"
+      >
+        <Link to="/" className="btn-primary">Go to homepage</Link>
+      </Banner>
+    </Hero>
   )
 }
 
--- src/pages/Rooms.jsx
+++ src/pages/Rooms.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import { Link } from 'react-router-dom'
+import Banner from '../components/Banner'
 const Rooms = () => {
   return (
-    <Hero hero="roomsHero"></Hero>
+    <Hero hero="roomsHero">
+      <Banner
+        title="Our Rooms"
+      >
+        <Link to="/" className="btn-primary">Home</Link>
+      </Banner>
+    </Hero>
   )
 }
 
